fe4f13f77b2cf6ea0c5bff34ec55b665cc4273b7
andrewtomash <kpubedko@gmail.com>
2017-02-05 23:19:27 +0200

    Interpreter kata solved

---
+++ src/codewars/kyu2/Interpreter.java
@@ -0,0 +1,73 @@
+package codewars.kyu2;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Andrew on 05.02.2017.
+ */
+public class Interpreter {
+    private HashMap<Character, String> variableValueMap = new HashMap<>();
+
+    public Double input(String input) {
+        String noSpaceInput = input.replaceAll(" ", "");
+        if (noSpaceInput.isEmpty()) {
+            return null;
+        } else if (isInvalid(input)) {
+            throw new RuntimeException();
+        }
+        return calculateInRecursion(noSpaceInput);
+
+    }
+
+    private double calculateInRecursion(String expression) {
+        if (isExpressionNoVariables(expression)) {
+            return calculate(expression);
+        } else if (matchesVariable(expression)) {
+            Character variable = expression.charAt(0);
+            Double result = calculateInRecursion(expression.substring(2));
+            variableValueMap.put(variable, String.valueOf(result));
+            return result;
+        } else if (isInParenthesis(expression)) {
+            return calculateInRecursion(expression.substring(1, expression.length() - 1));
+        } else {
+            for (Character character : variableValueMap.keySet()) {
+                expression = expression.replaceAll(String.valueOf(character), variableValueMap.get(character));
+            }
+            return calculate(expression);
+        }
+    }
+
+
+    private double calculate(String expression) {
+        String result = null;
+        try {
+            ScriptEngineManager mgr = new ScriptEngineManager();
+            ScriptEngine engine = mgr.getEngineByName("JavaScript");
+            result = engine.eval(expression).toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return Double.valueOf(result);
+    }
+
+    private boolean matchesVariable(String input) {
+        Pattern pattern = Pattern.compile("^[A-Za-z_]{1}[=]{1}.+");
+        return pattern.matcher(input).find();
+    }
+
+    private boolean isExpressionNoVariables(String input) {
+        return Pattern.compile("^[\\d\\+\\-\\*\\%\\/\\(\\)]+$").matcher(input).find();
+    }
+
+    private boolean isInParenthesis(String input) {
+        return Pattern.compile("^[\\(].+[\\)]$").matcher(input).find();
+    }
+
+    private boolean isInvalid(String input) {
+        return Pattern.compile("[\\d\\_]+ [\\d\\_]+").matcher(input).find();
+    }
+
+}
---
+++ src/tests/ExampleInterpreterTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import codewars.kyu2.Interpreter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+public class ExampleInterpreterTest {
+
+    @Test
+    public void basicTests() {
+        Interpreter interpreter = new Interpreter();
+
+        // Basic arithmetic
+        assertEquals(2, interpreter.input("1 + 1"), 0.0);
+        assertEquals(1, interpreter.input("2 - 1"), 0.0);
+        assertEquals(6, interpreter.input("2* 3"), 0.0);
+        assertEquals(2, interpreter.input("8 / 4"), 0.0);
+        assertEquals(3, interpreter.input("7 % 4"), 0.0) ;
+        assertEquals(2, interpreter.input("(7 + 3) / (2 * 2 + 1)"), 0.0);
+        assertEquals(6, interpreter.input("(8 - (4 + 2)) * 3"), 0.0);
+        assertEquals( 15, interpreter.input("(10 / (8 - (4 + 2))) * 3"), 0.0);
+        assertEquals( 7, interpreter.input("x = 7"), 0.0);
+        assertEquals( 7, interpreter.input("x"), 0.0);
+        assertEquals( 10, interpreter.input("x + 3"), 0.0);
+        assertEquals(12, interpreter.input("y = x + 5"), 0.0);
+        assertEquals( 12, interpreter.input("y"), 0.0);
+
+        // Variables
+        assertEquals(1, interpreter.input("x = 1"), 0.0);
+        assertEquals(1, interpreter.input("x"), 0.0);
+        assertEquals(4, interpreter.input("x + 3"), 0.0);
+//        assertFail("input: 'y'", () -> interpreter.input("y"));
+    }
+
+    private static void assertFail(String msg, Runnable runnable) {
+        try {
+            runnable.run();
+            fail(msg);
+        } catch (Exception e) {
+            // Ok
+        }
+    }
+}
